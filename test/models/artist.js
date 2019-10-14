07fd2685ebf0771f65d07bb3fcbf0da7096b1756
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-10-14 12:25:03 +0300

    synchronous teardown

--- test/models/artist.js
+++ test/models/artist.js
@@ -4,17 +4,28 @@ const expect = require('chai').expect
 const Artist = require('../../models/index').Artist
 
 describe('Artist', () => {
+  var createdRecords = []
+
   describe('#songs', () => {
-    it('should return an empty list when there are no songs', () => {
+    // Cause this to be a synchronous function by passing in 'done'.
+    it('should return an empty list when there are no songs', (done) => {
       Artist.create().done(artist => {
+        createdRecords.push(artist)
         artist.getSongs().done(songs => {
           expect(songs).to.eql([])
+          done()
         })
       })
     })
 
     it('should return an array of songs when the artist has songs', () => {
-      
+
+    })
+  })
+
+  after('clear created records', () => {
+    createdRecords.forEach(record => {
+      record.destroy()
     })
   })
 })
