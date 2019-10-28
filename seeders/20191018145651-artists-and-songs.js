779bf0d37d5f38c647d94ac218c88ead7718c5c2
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-10-28 12:02:40 +0200

    fix seeder syntax

--- seeders/20191018145651-artists-and-songs.js
+++ seeders/20191018145651-artists-and-songs.js
@@ -1,24 +1,15 @@
 'use strict';
 
 module.exports = {
-  async up: (queryInterface, Sequelize) => {
-    /*
-    Add altering commands here.
-    Return a promise to correctly handle asynchronicity.
-    
-    Example:
-    return queryInterface.bulkInsert('People', [{
-      name: 'John Doe',
-      isBetaMember: false
-    }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
     const transaction = await queryInterface.sequelize.transaction();
     try {
+      let now = new Date()
       await queryInterface.bulkInsert('Artists', [
-        { name: 'Billy Bob' },
-        { name: 'Skinny Pete' },
-        { name: 'Slim Sue' },
-        { name: 'Jumpin\' Jane' }
+        { name: 'Billy Bob', createdAt: now, updatedAt: now },
+        { name: 'Skinny Pete', createdAt: now, updatedAt: now },
+        { name: 'Slim Sue', createdAt: now, updatedAt: now },
+        { name: 'Jumpin\' Jane', createdAt: now, updatedAt: now }
       ], { transaction })
       await transaction.commit();
     } catch (err) {
