2e9fb2a91182278247687283bea211f40ec42594
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-11-24 13:11:12 +0200

    fixes for eslint

--- test/models/artist.js
+++ test/models/artist.js
@@ -6,42 +6,41 @@ const Artist = db.Artist
 const Song = db.Song
 
 describe('Artist', () => {
-
   const clearData = (done) => {
     return db.Song.destroy({ where: {}, force: true })
-    .then(() => {
-      db.Artist.destroy({ where: {}, force: true })
-    })
+      .then(() => {
+        db.Artist.destroy({ where: {}, force: true })
+      })
   }
 
   describe('#getSongs()', () => {
     // Make this a synchronous function by passing in 'done'.
     it('should return an empty list when there are no songs', (done) => {
       Artist.create({ name: 'Bob' })
-      .then(artist => artist.getSongs())
-      .then(songs => {
+        .then(artist => artist.getSongs())
+        .then(songs => {
           expect(songs).to.eql([])
           done()
-      })
+        })
     })
 
     it('should return an array of songs when the artist has songs', (done) => {
       let songArtist = null;
       Artist.create({ name: 'Brian' })
-      .then(artist => {
-        songArtist = artist
-        return Song.create({
-          title: 'A good song',
-          text: 'This is a song',
-          ArtistId: artist.id
-        }, {
-          include: [{ model: db.Artist }]
+        .then(artist => {
+          songArtist = artist
+          return Song.create({
+            title: 'A good song',
+            text: 'This is a song',
+            ArtistId: artist.id
+          }, {
+            include: [{ model: db.Artist }]
+          })
+        }).then(song => songArtist.countSongs())
+        .then(count => {
+          expect(count).to.eql(1)
+          done()
         })
-      }).then(song => songArtist.countSongs())
-      .then(count => {
-        expect(count).to.eql(1)
-        done()
-      })
     })
 
     beforeEach('clear all records', (done) => {
@@ -52,4 +51,4 @@ describe('Artist', () => {
       clearData().then(() => done())
     })
   })
-})
+}
