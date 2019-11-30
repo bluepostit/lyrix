e8036b6041a94cde309e1ed497898905a55708d4
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-11-30 17:33:35 +0200

    basic front page in React client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const testCallApi = async () => {
@@ -7,15 +8,39 @@ const testCallApi = async () => {
   return json
 }
 
+function CategoryCard (props) {
+  return (
+    <div className="card-category">
+      <h3>{props.title}</h3>
+      <p>{props.subtitle}</p>
+    </div>
+  )
+}
+
 
 function App() {
-  testCallApi()
-    .then(json => {
-      document.querySelector('body').innerHTML =
-        `<h1>There are ${json.data.length} songs in the database</h1>`    
-    })
+  // testCallApi()
+  //   .then(json => {
+  //     document.querySelector('body').innerHTML =
+  //       `<h1>There are ${json.data.length} songs in the database</h1>`
+  //   })
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="container banner-vcenter d-flex flex-column justify-content-center">
+        <div>
+          <h1>Welcome to Lyrix!</h1>
+          <p>Your lyrics managing companion</p>
+          <div className="card-category-wrapper col-12 col-lg-6 offset-lg-3">
+            <CategoryCard title="Artists"
+                          link="/artists"
+                          subtitle="All my people right here right now" />
+            <CategoryCard title="Songs" link="/songs" />
+            <CategoryCard title="Sets" />
+            <CategoryCard title="Gigs" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
