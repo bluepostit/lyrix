d93b3bd24cdde764350137348db98e5181da1646
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-12-07 19:56:52 +0200

    convert snake -> camel case

--- test/record-manager.js
+++ test/record-manager.js
@@ -1,9 +1,16 @@
 const path = require('path')
 
+const toSnakeCase = (camel) => {
+  const re = /([a-z])([A-Z])/g
+  return camel.replace(re, '$1-$2').toLowerCase()
+}
+
 class RecordManager {
   static async deleteAll (className) {
-    const modelPath = path.join('..', 'models', className.toLowerCase())
+    const moduleName = toSnakeCase(className)
+    const modelPath = path.join('..', 'models', moduleName)
     const modelClass = require(modelPath)
+    // console.log(`Deleting all: ${className}`)
 
     await modelClass.query().delete()
   }
