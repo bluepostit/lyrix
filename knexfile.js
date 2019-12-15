52ba4cc4241d7ec8351d00ff90b8ae02166ceb1f
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-12-15 12:45:44 +0200

    use improved config module

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,5 @@
 const path = require('path')
-const env = process.env.NODE_ENV || 'development'
-const config = require(path.join(__dirname, 'config/config'))[env]
+const config = require(path.join(__dirname, 'config'))
 
 module.exports = {
 
