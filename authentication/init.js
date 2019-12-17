f9c55d29ffd49bd70ba9cdb83484fd991dbac189
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-12-17 14:43:49 +0200

    fix serializing functions for passport

--- authentication/init.js
+++ authentication/init.js
@@ -26,7 +26,6 @@ passport.use(new LocalStrategy(
 
         const match = await user.checkPassword(password)
         console.log('did the password match? ' + match)
-        console.log(user)
         if (match) {
           return callback(null, user)
         } else {
@@ -36,20 +35,19 @@ passport.use(new LocalStrategy(
   }
 ))
 
-passport.serializeUser = (user, callback) => {
+passport.serializeUser((user, callback) => {
   console.log('serializeUser')
   callback(null, user.id)
-}
+})
 
-passport.deserializeUser = async (id, callback) => {
-  const user = await User.findById(id)
-  // if (user) {
+passport.deserializeUser(async (id, callback) => {
+  const user = await User.query().findById(id)
+  if (user) {
     callback(null, user)
-  // } else {
-
-  //   return callback(`Could not deserialize user ${id}`)
-  // }
-}
+  } else {
+    return callback(new Error(`Could not deserialize user #${id}`))
+  }
+})
 
 passport.authenticationMiddleware = () => {
   require('./ensure-logged-in')
