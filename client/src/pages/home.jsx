0056dac8669cb54783edf4fc2dc0d82ac085d448
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-12-29 11:33:14 +0200

    home page - split small, large render code

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { MEDIA_CLASS_SMALL, MEDIA_CLASS_LARGE } from '../common'
 
 
 const pluralize = (number, noun) => {
@@ -24,6 +25,63 @@ function CategoryCard (props) {
   )
 }
 
+const handleMenuButtonClick = (e) => {
+  console.log(`Clicked`)
+  console.log(e.target.dataset.action)
+}
+
+/**
+ * Menu button for small screen's home page
+ */
+const MenuButton = (props) => {
+  const action = props.action
+  const title = action[0].toUpperCase() + action.slice(1)
+
+  return (
+    <button className="btn btn-primary"
+            data-action={action}
+            onClick={handleMenuButtonClick}>{title}
+    </button>
+  )
+}
+
+const SmallScreenContent = (props) => {
+  return (
+    <div className={MEDIA_CLASS_SMALL}>
+      <div className="menu-title">Lyrix</div>
+      <div className="menu-buttons">
+        <MenuButton action="arrange" />
+        <MenuButton action="practice" />
+        <MenuButton action="perform" />
+      </div>
+    </div>
+  )
+}
+
+const BigScreenContent = (props) => {
+  return (
+    <div className={MEDIA_CLASS_LARGE}>
+      <div className="container banner-vcenter d-flex flex-column justify-content-center">
+        <div>
+          <h1>Welcome to Lyrix!</h1>
+          <p>Your lyrics managing companion</p>
+          <div className="card-category-wrapper col-12 col-lg-6 offset-lg-3">
+            <CategoryCard title="Artists"
+                          link="/artists"
+                          subtitle="All my people, right here right now" />
+            <CategoryCard title="Songs"
+                          link="/songs"
+                          subtitle={`We have ${pluralize(props.songCount, 'song')}`} />
+            <CategoryCard title="Sets" />
+            <CategoryCard title="Gigs" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
 const Home = (props) => {
   const [songCount, setSongCount] = useState(0)
 
@@ -31,21 +89,9 @@ const Home = (props) => {
     .then(count => setSongCount(count))
 
   return (
-    <div className="container banner-vcenter d-flex flex-column justify-content-center">
-      <div>
-        <h1>Welcome to Lyrix!</h1>
-        <p>Your lyrics managing companion</p>
-        <div className="card-category-wrapper col-12 col-lg-6 offset-lg-3">
-          <CategoryCard title="Artists"
-                        link="/artists"
-                        subtitle="All my people, right here right now" />
-          <CategoryCard title="Songs"
-                        link="/songs"
-                        subtitle={`We have ${pluralize(songCount, 'song')}`} />
-          <CategoryCard title="Sets" />
-          <CategoryCard title="Gigs" />
-        </div>
-      </div>
+    <div className="home-page">
+      <SmallScreenContent />
+      <BigScreenContent songCount={songCount} />
     </div>
   )
 }
