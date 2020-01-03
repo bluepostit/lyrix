0589a94fc85baed71049ce24064dfdc17eb327e9
bluepostit <32864499+bluepostit@users.noreply.github.com>
2020-01-03 14:13:34 +0200

    use redis url if present

--- session.js
+++ session.js
@@ -3,12 +3,24 @@ const redis = require('redis')
 const connectRedis = require('connect-redis')
 const RedisStore = connectRedis(session)
 
+const createRedisClient = () => {
+  let client = null
+
+  const redisUrl = process.env.REDIS_URL
+  if (redisUrl) {
+    client = redis.createClient(redisUrl)
+  } else {
+    client = redis.createClient()
+  }
+  client.on('error', (err) => {
+    console.log('Redis error: ', err)
+  })
+  return client
+}
+
 module.exports = {
   init: (app, config) => {
-    const redisClient = redis.createClient()
-    redisClient.on('error', (err) => {
-      console.log('Redis error: ', err)
-    })
+    const redisClient = createRedisClient()
 
     // Session
     app.use(session({
@@ -18,8 +30,6 @@ module.exports = {
       saveUninitialized: true,
       cookie: { secure: false },
       store: new RedisStore({
-        host: 'localhost',
-        port: 6379,
         client: redisClient,
         ttl: 86400
       })
