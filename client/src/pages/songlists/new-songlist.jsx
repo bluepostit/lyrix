482819bc59cae36637b30f0358d0bed921f13a75
bluepostit <32864499+bluepostit@users.noreply.github.com>
2020-04-11 17:10:30 +0300

    rm formik usage

--- client/src/pages/songlists/new-songlist.jsx
+++ client/src/pages/songlists/new-songlist.jsx
@@ -1,47 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from "react-router-dom"
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-
 import { MobileHeader } from '../../components'
 import { MEDIA_CLASS_SMALL, MEDIA_CLASS_LARGE } from '../../common'
 
 const SmallScreenContent = (props) => {
-  const onFormSubmit = async (values, { setSubmitting }) => {
-    fetch('/songlists', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: JSON.stringify(values)
-    })
-    .then(response => response.json())
-    .then((data) => {
-      setSubmitting(false)
-      console.log(data)
-      // TO DO: handle server errors!
-      if (data.status === 200) {
-        console.log('created list')
-        // TO DO: redirect to show the newly created list
-      }
-    })
-  }
-
   return (
     <div className={MEDIA_CLASS_SMALL}>
       <div className="list-page">
         <MobileHeader title={props.title} />
-        <Formik
-          initialValues={{ title: '' }}
-          onSubmit={onFormSubmit}  
-        >
-          {({ errors, isSubmitting }) => (
-            <Form>
-              <Field type="text" name="title" />
-              <ErrorMessage name="title" component="div" />
-              <button type="submit" disabled={isSubmitting}>Submit</button>
-            </Form>
-          )}
-        </Formik>
+        To Be Implemented
       </div>
     </div>
   )
