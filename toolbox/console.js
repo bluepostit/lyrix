b28f5a1557065f316ad9120695a8116459cce54d
Yair <32864499+bluepostit@users.noreply.github.com>
2020-07-13 13:38:22 +0300

    add console

--- toolbox/console.js
+++ toolbox/console.js
@@ -1,4 +1,14 @@
-// const knex = require('../db/knex')
+const repl = require('repl')
 const db = require('../models')
+const util = require('../util')
 
-module.exports = db
+console.log('--- Lyrix console ---')
+console.log(`
+You can use \`await\` with promises, eg.
+\`let artist = await db.Artist.query().findOne('name ', 'ilike', '%coldplay%')\`
+`)
+
+// Start the REPL
+const replServer = repl.start({})
+replServer.context.db = db
+replServer.context.util = util
