daf47564f02348fae02f3887e9bc9c9d828efd3f
Yair <32864499+bluepostit@users.noreply.github.com>
2020-07-31 14:19:52 +0300

    test: return current user's song items

--- test/behavior/song-items.js
+++ test/behavior/song-items.js
@@ -29,40 +29,83 @@ describe('/song-items', async () => {
     return agent
   }
 
-  it('should return an error when not logged in', async () => {
-    const res = await chai.request(app)
-      .get('/song-items')
+  describe('GET /', () => {
 
-    expect(res).to.have.status(200)
-    expect(res.body).to.have.status(401)
-    expect(res.body.error).to.not.be.empty
-  })
+    it('should return an error when not logged in', async () => {
+      const res = await chai.request(app).get('/song-items')
 
-  it('should return empty when the user has no song items', async () => {
-    const agent = await login()
-    const res = await agent
-      .get('/song-items')
-    agent.close()
+      expect(res).to.have.status(200)
+      expect(res.body).to.have.status(401)
+      expect(res.body.error).to.not.be.empty
+    })
 
-    expect(res).to.have.status(200)
-    expect(res.body).to.be.an('object')
-    expect(res.body.error).to.be.eql(false)
-    expect(res.body.data).to.be.empty
-  })
+    it('should return empty when the user has no song items', async () => {
+      const agent = await login()
+      const res = await agent.get('/song-items')
+      agent.close()
 
-  it('should not return song items belonging to other users', async () => {
-    await RecordManager.loadFixture('song-items.only-other-user')
+      expect(res).to.have.status(200)
+      expect(res.body).to.be.an('object')
+      expect(res.body.error).to.be.eql(false)
+      expect(res.body.data).to.be.empty
+    })
 
-    const allItemsCount = await SongItem.query().resultSize()
-    expect(allItemsCount).to.be.greaterThan(0)
+    it('should not return song items belonging to other users', async () => {
+      await RecordManager.loadFixture('song-items.only-other-user')
 
-    const agent = await login()
-    const res = await agent
-      .get('/song-items')
-    agent.close()
+      const allItemsCount = await SongItem.query().resultSize()
+      expect(allItemsCount).to.be.greaterThan(0)
 
-    expect(res).to.have.status(200)
-    expect(res.body).to.be.an('object')
-    expect(res.body.data).to.be.empty
+      const agent = await login()
+      const res = await agent.get('/song-items')
+      agent.close()
+
+      expect(res).to.have.status(200)
+      expect(res.body).to.be.an('object')
+      expect(res.body.data).to.be.empty
+    })
+
+    it('should return song items belonging to the current user', async () => {
+      await RecordManager.loadFixture('songs')
+      await RecordManager.loadFixture('song-item-types')
+      const data = [
+        {
+          title: 'Chords for Song',
+          text: 'Some funky chords',
+          song_id: 1,
+          song_item_type_id: 1
+        },
+        {
+          title: 'Chords for Song 2',
+          text: 'Some funky chords',
+          song_id: 2,
+          song_item_type_id: 1
+        },
+        {
+          title: 'Info for Song 2',
+          text: 'Some interesting information',
+          song_id: 2,
+          song_item_type_id: 2
+        }
+      ]
+
+      const user = await RecordManager.insertUser()
+
+      await user.$relatedQuery('songItems')
+        .insertGraph(data)
+
+      const agent = await login(user)
+      const res = await agent.get('/song-items')
+      agent.close()
+
+      expect(res).to.have.status(200)
+      expect(res.body).to.be.an('object')
+      const items = res.body.data
+      expect(items.length).to.be.eql(data.length)
+      data.forEach((o, index) => {
+        expect(items[index].title).to.eql(o.title)
+        expect(items[index].song_id).to.eql(o.song_id)
+      })
+    })
   })
 })
