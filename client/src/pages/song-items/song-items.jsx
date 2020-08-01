768c6a02676b16b21da546081d9f258af7d94ffb
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-01 19:56:29 +0300

    rm incorrect link

--- client/src/pages/song-items/song-items.jsx
+++ client/src/pages/song-items/song-items.jsx
@@ -9,10 +9,6 @@ const SongItems = () => {
     history.push(`/song-items/${songItem.id}`)
   }
 
-  const onNewClick = () => {
-    history.push('/song-items/new')
-  }
-
   const renderSongItem = (songItem) => {
     return <SongItem songItem={songItem} />
   }
@@ -20,7 +16,6 @@ const SongItems = () => {
   return (
     <ItemListPage title="My song items"
               getItems='/song-items'
-              onNewClick={onNewClick}
               onItemClick={onSongItemClick}
               renderItem={renderSongItem}
     />
