2b5584910f814052b5a41b3f6559b67f7aa52a52
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-03 22:35:57 +0300

    rm debug statement

--- authentication/ensure-logged-in.js
+++ authentication/ensure-logged-in.js
@@ -4,7 +4,6 @@ module.exports = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next()
   }
-  console.log('not logged in! redirecting now')
   res.json({
     status: StatusCodes.UNAUTHORIZED,
     error: 'You must log in to access this'
