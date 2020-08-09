0a177a2998dc2f0e2b61eefe4cb29d77ea7a0965
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-09 09:40:01 +0300

    show deleter modal for artist

--- client/src/pages/empty-page.jsx
+++ client/src/pages/empty-page.jsx
@@ -3,13 +3,15 @@ import { Page } from './page'
 
 const EmptyPage = ({
   title,
-  actions
+  actions,
+  children
 }) => {
   return (
     <Page title={title} actions={actions}>
       <div className="page-content list-page">
         <div className="lyrix-list empty">
           <h3>Nothing to show here, yet.</h3>
+          {children}
         </div>
       </div>
     </Page>
