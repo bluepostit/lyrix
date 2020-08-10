b655565daf03bd29960fdcd586d484d0491adf33
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-10 11:08:34 +0300

    much improved navbar; better responsive design

--- client/src/components/headers.jsx
+++ client/src/components/headers.jsx
@@ -4,84 +4,6 @@ import { NavbarButton } from '../components/buttons'
 import { Icon } from '../components/icons'
 import { Nav, Navbar } from 'react-bootstrap'
 
-/**
- *
- * @param {*} props object containing the following:
- * - title - (any) content to be displayed in the center of the navbar
- * - actions - (array) OPTIONAL contextual actions to offer the user
- */
-const LyrixNavbar = ({
-  title,
-  actions = []
-}) => {
-  const history = useHistory()
-  const [expanded, setExpanded] = useState(false)
-
-  const goHome = () => {
-    history.push('/')
-  }
-
-  const execute = (action) => {
-    if (typeof action === 'string') {
-      history.push(action)
-    } else if (typeof action === 'function') {
-      action()
-    }
-  }
-
-  const handleActionClick = (action) => {
-    setExpanded(false)
-    if (!action || !action.value) {
-      return
-    }
-    execute(action.value)
-  }
-
-  const collapse = () => {
-    setTimeout(() => {
-      setExpanded(false)
-    }, 10)
-  }
-
-  const onToggle = (expanded) => {
-    if (!expanded) {
-      collapse()
-    } else {
-      setExpanded(expanded)
-    }
-  }
-
-  const className = 'lyrix-navbar ' + (expanded ? '' : 'collapsed')
-  const visibleActions = actions.filter(i => i.value)
-
-  return (
-    <Navbar collapseOnSelect fixed="top"
-      onToggle={onToggle}
-      expanded={expanded}
-      expand="lg" variant="light" className={className}>
-      <div className="navbar-title">
-        <Navbar.Brand>{title}</Navbar.Brand>
-      </div>
-
-        <Navbar.Toggle aria-controls="lyrix-navbar-more" />
-        <Navbar.Collapse id="lyrix-navbar-more">
-          <Nav className="mr-auto">
-            <Nav.Link onClick={goHome} >
-              <Icon entity="home" /><strong> Lyrix</strong>
-            </Nav.Link>
-            <div className="horizontal-divider"
-              hidden={visibleActions.length < 1} />
-            {visibleActions.map((action, index) =>
-              <NavbarButton action={action}
-                onClick={handleActionClick}
-                key={index + 1} />
-            )}
-          </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-  )
-}
-
 const SongItemPageTitle = ({
   songItem = { title: '' },
   title = 'Add a New Song Item'
@@ -93,6 +15,52 @@ const SongItemPageTitle = ({
       <strong className="ml-2">{songItem.title || title}</strong>
     </div>
   )
+}
+
+const LyrixNavbar = ({ title, actions = [] }) => {
+  const history = useHistory()
+  const visibleActions = actions.filter(i => i.value)
+
+  const goHome = () => {
+    history.push('/')
+  }
+
+  const handleActionClick = (action) => {
+    if (!action || !action.value) {
+      return
+    }
+    execute(action.value)
+  }
+
+  const execute = (action) => {
+    if (typeof action === 'string') {
+      history.push(action)
+    } else if (typeof action === 'function') {
+      action()
+    }
+  }
+
+  return (
+    <Navbar collapseOnSelect variant="light" fixed="top"
+        className="lyrix-navbar" expand="md">
+      <Navbar.Brand>{title}</Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="lyrix-navbar-links" />
+      <Navbar.Collapse id="lyrix-navbar-links">
+        <Nav className="mr-auto">
+          <Nav.Link onClick={goHome} >
+            <Icon entity="home" /><strong> Lyrix</strong>
+          </Nav.Link>
+          {visibleActions.map((action, index) =>
+            <NavbarButton action={action}
+              onClick={handleActionClick}
+              key={index + 1} />
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+
 }
 
 export { LyrixNavbar as Navbar, SongItemPageTitle }
