fbbc195e50ed0641e959c7ed5f6eee9a25f6a10b
Yair <32864499+bluepostit@users.noreply.github.com>
2021-07-13 20:04:29 +0300

    bring in old, better index code

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.scss';
-import App from './App.jsx';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
